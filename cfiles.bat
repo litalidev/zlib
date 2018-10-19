3e418b71951e3c4c06a9d5df6e287a211f96795f
Lita Li <litalidev@gmail.com>
2018-10-19 16:25:45 +0800

    cfiles: use _MSC_VER as filename instead of visual studio version (e.g. 15.8.3)

--- cfiles.bat
+++ cfiles.bat
@@ -1,23 +1,24 @@
 @echo off
-rem cfiles name version arch  build [tool tver] -s src1 [src2 ...]
-rem cfiles zlib 1.2.11  x86   mtd   [v141 15.8] -s include lib\zlibstaticd.lib
+rem cfiles name version arch  build [tver msvc] -s src1 [src2 ...]
+rem cfiles zlib 1.2.11  x86   mtd   [v141 1915] -s include lib\zlibstaticd.lib
 rem cfiles %1   %2      %3    %4    %5
 rem
 rem name=project name, e.g. zlib
 rem vesion=project version, e.g. 1.2.11
-rem tool=platform tool version, v141=vs2017
+rem tver=platform tool version, v141=vs2017
+rem msvc=msvc version, indicated by c++ macro, _MSC_VER
 rem ipath=temp installed path, e.g. p:\prj\libs\zlib\1.2.11\tmp
 rem
-rem filename: zlib-1.2.11-v141-15.8-x86-mdd.lib
-rem filename: %name%-%version%-%tool%-%tver%-%arch%-%build%.lib
+rem filename: zlib-1.2.11-v141-1915-x86-mdd.lib
+rem filename: %name%-%version%-%tver%-%msvc%-%arch%-%build%.lib
 rem
 rem
 rem cfiles zlib  1.2.11 x86 mdd -s include lib\zlibstaticd.lib
 rem
 
 rem default values
-set tool=v141
-set tver=15.8
+set tver=v141
+set msvc=1915
 
 if [%1] == [] goto error1
 if [%1] == [-s] goto error1
@@ -41,12 +42,12 @@ shift
 
 if [%1] == [] goto argend1
 if [%1] == [-s] goto src
-set tool=%1
+set tver=%1
 shift
 
 if [%1] == [] goto argend1
 if [%1] == [-s] goto src
-set tver=%1
+set msvc=%1
 shift
 
 :src
@@ -75,7 +76,7 @@ for /f %%i in ( "%dname%" ) do set dfolder=%%~di%%~pi&set dext=%%~xi
 rem echo dname=%dname%
 rem echo dfolder=%dfolder%
 rem echo dext=%dext%
-set dfname=%name%-%version%-%tool%-%tver%-%arch%-%build%%dext%
+set dfname=%name%-%version%-%tver%-%msvc%-%arch%-%build%%dext%
 rem echo dfname=%dfname%
 
 rem echo xcopy /i/e/y %base%%1 %dfolder%%dfname%
